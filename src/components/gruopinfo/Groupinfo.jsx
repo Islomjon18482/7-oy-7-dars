dfc5a87192a7671bb49825cb06c378fb3f6148c9
Islomjon Abduganiyev <islomjonabduganiyev@Islomjons-MacBook-Air.local>
2024-03-26 13:06:54 +0500

    Jsx codes wroten

--- src/components/gruopinfo/Groupinfo.jsx
+++ src/components/gruopinfo/Groupinfo.jsx
@@ -1,7 +1,57 @@
 import React from "react";
 import styles from "./groupinfo.module.css";
+import { useState, useRef } from "react";
+
 
 function Groupinfo() {
+  const [show, setShow] = useState(false)
+  const nameRef = useRef('')
+  const lastNameRef = useRef('')
+  const fatherNameRef = useRef('')
+  const telRef = useRef('')
+
+  function validate() {
+    const isNameValid = nameRef.current.value.trim() !== '';
+    const isLastNameValid = lastNameRef.current.value.trim() !== '';
+    const isFatherNameValid = fatherNameRef.current.value.trim() !== '';
+    const isTelValid = telRef.current.value.trim() !== '';
+  
+    return isNameValid && isLastNameValid && isFatherNameValid && isTelValid;
+  }
+  
+
+  function addStudent(){
+    if(validate){
+      const studentData = {
+        name: nameRef.current.value.trim(),
+        lastName: lastNameRef.current.value.trim(),
+        fatherName: fatherNameRef.current.value.trim(),
+        tel: telRef.current.value.trim(),
+      };
+  
+      const existingStudentData = JSON.parse(localStorage.getItem('student')) || [];
+  
+      existingStudentData.push(studentData)
+  
+      localStorage.setItem('student', JSON.stringify(existingStudentData));
+      nameRef.current.value = ''
+      lastNameRef.current.value = ''
+      fatherNameRef.current.value = ''
+      telRef.current.value = ''
+    }
+  }
+
+  
+
+  function openModal(e){
+    e.stopPropagation()
+    setShow(!show)
+  }
+  window.addEventListener('click', function(){
+    if(show){
+      setShow(false)
+    }
+  })
   return (
     <>
       <div className={styles.info__wrapper}>
@@ -10,7 +60,7 @@ function Groupinfo() {
             <img src="./search.svg" />
             <input type="text" placeholder="Qidiruv" />
           </div>
-          <button>
+          <button onClick={(e) =>openModal(e)}>
             <img src="./add.svg" />
             <span>O'quvchi qo'shish</span>
           </button>
@@ -94,34 +144,34 @@ function Groupinfo() {
           </div>
         </div>
       </div>
-      <div className={styles.modal}>
+      {show && <div onClick={(e) => e.stopPropagation()} className={styles.modal}>
         <div className={styles.modal__heading}>
           <h2>Guruhga yanigi o'quvchi qo'shish</h2>
         </div>
         <div className={styles.modal__inp}>
           <label>
             <p>O‘quvchi ismi</p>
-            <input type="text" placeholder="Ism kiriting..."/>
+            <input ref={nameRef} type="text" placeholder="Ism kiriting..."/>
           </label>
           <label>
             <p>O‘quvchi familiyasi</p>
-            <input type="text" placeholder="Familiya kiriting..."/>
+            <input ref={lastNameRef} type="text" placeholder="Familiya kiriting..."/>
           </label>
           <label>
             <p>Otasining ismi</p>
-            <input type="text" placeholder="Otasining ismini kiriting..."/>
+            <input ref={fatherNameRef} type="text" placeholder="Otasining ismini kiriting..."/>
           </label>
           <label>
             <p>Telefon raqami</p>
-            <input type="text" placeholder="+998"/>
+            <input ref={telRef} type="text" placeholder="+998"/>
           </label>
           <label>
             <p>Tug'ilgan sanasi</p>
             <input type="text" placeholder="kk.oo.yyyy"/>
           </label>
         </div>
-        <button>O'quvchi qo'shish</button>
-      </div>
+        <button onClick={addStudent}>O'quvchi qo'shish</button>
+      </div>}
     </>
   );
 }
